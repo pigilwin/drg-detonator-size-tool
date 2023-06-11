4a9add5989063de7744554583b5d2349b363f591
Tim Remnant <timwebdesign2013@gmail.com>
2023-06-11 19:39:57 +0100

    Initial commit

---
+++ Source/FSD/Private/FlatDamageUpgrade.cpp
@@ -0,0 +1,14 @@
+#include "FlatDamageUpgrade.h"
+#include "Templates/SubclassOf.h"
+
+FUpgradeValues UFlatDamageUpgrade::GetUpgradedValue(TSubclassOf<AActor> Item, AFSDPlayerState* Player, UDamageClass* NewDamageClass) {
+    return FUpgradeValues{};
+}
+
+UFlatDamageUpgrade::UFlatDamageUpgrade() {
+    this->Damage = 0.00f;
+    this->DamageClass = NULL;
+    this->RequiredClass = NULL;
+    this->Condition = NULL;
+}
+
